aa013d94ee5fc9dcf1087adfe940c141150aad95
nnsnodnb <ahr63_gej@me.com>
2018-04-18 20:55:14 +0900

    Add HKWorkout query

--- LiveActivity/ViewController.swift
+++ LiveActivity/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
+    private let healthStore = HKHealthStore()
+    let readDataTypes: Set<HKObjectType> = [
+        HKWorkoutType.workoutType(),
+        HKObjectType.quantityType(forIdentifier: .heartRate)!,
+        HKObjectType.quantityType(forIdentifier: .bodyTemperature)!,
+        HKObjectType.quantityType(forIdentifier: .respiratoryRate)!
+    ]
+
+    private var workouts = [HKWorkout]()
+
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        requestAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    private func requestAuthorization() {
+        healthStore.requestAuthorization(toShare: nil, read: readDataTypes) { [unowned self] (success, error) in
+            guard success, error == nil else {
+                return
+            }
+            self.getWorkouts()
+        }
+    }
 
+    private func getWorkouts() {
+        let predicate =  HKQuery.predicateForWorkouts(with: .other)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        let sampleQuery = HKSampleQuery(sampleType: HKWorkoutType.workoutType(), predicate: predicate, limit: 0, sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+            guard let workouts = samples as? [HKWorkout], error == nil else {
+                return
+            }
+            self.workouts = workouts
+        }
+        healthStore.execute(sampleQuery)
+    }
 }
-
