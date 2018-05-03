2cff7244c57325725a9cb7f07be06bbf991a7845
nnsnodnb <ahr63_gej@me.com>
2018-05-03 16:33:59 +0900

    Add DetailViewController.swift

---
+++ LiveActivity/Script/ViewController/Detail/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  LiveActivity
+//
+//  Created by Oka Yuya on 2018/05/03.
+//  Copyright © 2018年 Oka Yuya. All rights reserved.
+//
+
+import UIKit
+import Charts
+import HealthKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
